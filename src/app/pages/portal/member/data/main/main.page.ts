b0c69c9a5d1781799824c9f44c99f0cf5a486732
Mobile-Box <info@mobile-box.eu>
2022-05-13 12:41:10 +0200

    13.05.22 12:40

--- src/app/pages/portal/member/data/main/main.page.ts
+++ src/app/pages/portal/member/data/main/main.page.ts
@@ -16,6 +16,12 @@ export class MainPage implements OnInit {
   public maxInputIban = environment.maxInputIban;
   public maxInputSocialSecurityNumber = environment.maxInputSocialSecurityNumber;
   public maxInputTaxIdentification = environment.maxInputTaxIdentification;
+  public maxInputGrossSalary = environment.maxInputGrossSalary;
+  public maxInputWagePerHour = environment.maxInputWagePerHour;
+  public maxInputHoursPerWeek = environment.maxInputHoursPerWeek;
+  public maxInputBirthday = environment.maxInputBirthday;
+  public maxInputStart = environment.maxInputStart;
+  public maxInputEnd = environment.maxInputEnd;
 
   // Urls
   private kTeamMember = "37";
@@ -34,11 +40,13 @@ export class MainPage implements OnInit {
     cShippingStreetNumber: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
     cShippingZip: ['', [Validators.required, Validators.minLength(this.maxZip), Validators.maxLength(this.maxZip)]],
     cShippingCity: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
-
+    nHoursPerWeek: ['', [Validators.required, Validators.maxLength(this.maxInputHoursPerWeek)]],
+    nWagePerHour: ['', [Validators.required, Validators.maxLength(this.maxInputWagePerHour)]],
+    nGrossSalary: ['', [Validators.required, Validators.maxLength(this.maxInputGrossSalary)]],
     cTaxIdentification: ['', [Validators.required, Validators.maxLength(this.maxInputTaxIdentification)]],
     cHealthInsurance: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
     cSocialSecurtiyNumber: ['', [Validators.required, Validators.maxLength(this.maxInputSocialSecurityNumber)]],
-    cNationaltiy: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
+    cNationality: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
     tConfession: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
     cBank: ['', [Validators.required, Validators.maxLength(this.maxInput)]],
     cIban: ['', [Validators.required, Validators.maxLength(this.maxInputIban)]],
@@ -90,4 +98,60 @@ export class MainPage implements OnInit {
   onChange() {
 
   }
+
+  onSave() {
+    this.bSubmitted = true;
+
+    // check for invalid input
+    if (!this.fgTeamMember.valid || this.oState == null || this.oRelationship == null || this.oTitle == null) {
+      console.log(this.oState);
+      console.log(this.oRelationship);
+      console.log(this.oTitle);
+      this.alertInvalid();
+      return;
+    }
+
+    // prepare data
+    let collector =
+      {
+        cPreName: this.fgTeamMember.get('cPreName').value,
+        cSurName: this.fgTeamMember.get('cSurname').value,
+        cMail: this.fgTeamMember.get('cSurname').value,
+        cMailCompany: this.fgTeamMember.get('cMailCompany').value,
+        cTrelloUsername: this.fgTeamMember.get('cTrelloUsername').value,
+        cPhoneMobile: this.fgTeamMember.get('cPhoneMobile').value,
+        cPhoneFixedLine: this.fgTeamMember.get('cPhoneFixedLine').value,
+        cShippingStreet: this.fgTeamMember.get('cShippingStreet').value,
+        cShippingStreetNumber: this.fgTeamMember.get('cShippingStreetNumber').value,
+        cShippingZip: this.fgTeamMember.get('cShippingZip').value,
+        cShippingCity: this.fgTeamMember.get('cShippingCity').value,
+        nHoursPerWeek: this.fgTeamMember.get('nHoursPerWeek').value,
+        nWagePerHour: this.fgTeamMember.get('nWagePerHour').value,
+        nGrossSalary: this.fgTeamMember.get('nGrossSalary').value,
+        cTaxIdentification: this.fgTeamMember.get('cTaxIdentification').value,
+        cHealthInsurance: this.fgTeamMember.get('cHealthInsurance').value,
+        cSocialSecurtiyNumber: this.fgTeamMember.get('cSocialSecurtiyNumber').value,
+        cNationality: this.fgTeamMember.get('cNationality').value,
+        tConfession: this.fgTeamMember.get('tConfession').value,
+        cBank: this.fgTeamMember.get('cBank').value,
+        cIban: this.fgTeamMember.get('cIban').value,
+        nVacation: this.fgTeamMember.get('nVacation').value,
+
+      };
+    console.log(collector);
+
+    // send data
+    this.connApi.safePost(this.urlSave, collector).subscribe((data: HttpResponse<any>) => {
+      if (data.status == 200) {
+        this.toastSaved();
+      }
+    }, error => {
+      console.log(error);
+      if (error.status == 406) {
+        this.alertCollectorNameForgiven();
+      }
+    });
+
+  }
+
 }
